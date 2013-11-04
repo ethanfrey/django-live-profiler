1a95bb06e84a3b1a536234a71ae7aa33cc88a687
Ethan <ethan@orderbird.com>
2013-11-04 12:09:27 +0100

    Change version to 0.0.9a

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name = "django-live-profiler",
-    version = "0.0.9",
+    version = "0.0.9a",
     author = "Alexander Tereshkin",
     author_email = "atereshkin@invitebox.com",
     description = ("A database access profiler for Django-based applications that can be ran in production "
@@ -31,7 +31,7 @@ setup(
         "Topic :: Software Development",
         "Framework :: Django",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",        
+        "License :: OSI Approved :: BSD License",
     ],
     include_package_data=True,
         entry_points={
