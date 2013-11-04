fd8c445d526659d20126e31697088613a058637d
Ethan <ethan@orderbird.com>
2013-11-04 12:58:24 +0100

    Turn off logging of find, so it doesn't crash.  Better url fix.

--- profiler/instrument.py
+++ profiler/instrument.py
@@ -34,7 +34,7 @@ INSTRUMENTED = False
 
 ############ code for pymongo instrumentation #############
 
-def wrap_mongo(orig, query, force_refresh=False):
+def wrap_mongo(orig, query, instrument_cursor=False):
     def wrapper(self, *args, **kwargs):
         client = get_client()
         if client is None:
@@ -52,26 +52,27 @@ def wrap_mongo(orig, query, force_refresh=False):
                            view=_get_current_view(),
                            type='mongo')
             log_val = dict(time=(0.0 + d.seconds * 1000 + d.microseconds/1000), count=1)
-            print "Logging {} : {}".format(log_key, log_val)
             client.insert(log_key, log_val)
     return wrapper
 
 
-def find_action(collection, *args, **kwargs):
-    return u"{} : {}".format(collection.name, kwargs.get('spec', 'ALL'))
+def find_action(collection, spec={}, *args, **kwargs):
+    log_spec = spec.copy()
+    for key in log_spec.keys():
+        if not key == "_cls":
+            log_spec[key] = '?'
+    return u"{} : {}".format(collection.name, log_spec)
 
 def instrument_mongo():
     try:
         from pymongo.collection import Collection
-        print "initializing pymongo"
         Collection.save = wrap_mongo(Collection.save, 'save')
         Collection.insert = wrap_mongo(Collection.insert, 'insert')
         Collection.update = wrap_mongo(Collection.update, 'update')
         Collection.remove = wrap_mongo(Collection.remove, 'remove')
         Collection.count = wrap_mongo(Collection.count, 'count')
-        Collection.find = wrap_mongo(Collection.find, find_action, force_refresh=True)
+        Collection.find = wrap_mongo(Collection.find, find_action)
     except ImportError:
-        print "Failed to import pymongo"
         pass
 
 
